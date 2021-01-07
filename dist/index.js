6b7a95f02c0b09756a6855c0fff3df4aa1773ccb
Jay Jin <sildeswj@gmail.com>
2021-01-07 12:32:51 +0900

    aa

--- dist/index.js
+++ dist/index.js
@@ -14,19 +14,8 @@ const app = async () => {
     // const githubToken = core.getInput('github-token');
     // const githubRunId = core.getInput('github-run-id');
     let userData = core.getInput('user-data');
-    // const slackWebhookUrl = core.getInput('slack-webhook-url');
-
-
-    // console.log(`Input values ${githubToken}, ${slackWebhookUrl}, ${githubRunId}, ${userData}, ${userData2}!`);
-    // const result = `${githubToken}, ${slackWebhookUrl}, ${githubRunId}`
-
     userData = JSON.parse(userData)
-    // getReviewer({ userData, slackWebhookUrl });
-
-    // Get the JSON webhook payload for the event that triggered the workflow.
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
+    getReviewer({ userData, slackWebhookUrl });
   } catch (error) {
     core.setFailed(error);
   }
